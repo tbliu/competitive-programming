bf5ced4017624a9cc5b3942ba0b7d172c2037fab
Timothy <tim@lumosity.ai>
2025-12-02 07:37:54 -0800

    Working on part 2 day 2

--- advent-of-code/2025/day02.erl
+++ advent-of-code/2025/day02.erl
@@ -6,14 +6,94 @@ main(_) ->
     Lines = read_lines_string(),
     SumInvalid = sum_invalid(Lines),
     io:format("Part 1: ~p~n", [SumInvalid]),
+
+    % Part 2:
+    % - start with first digit; copy it as many times to get it into range. Add to SumInvalid
+    % - once you can no longer add digits, take the first two digits slice; copy it as many times to get it into range
+    % - repeat until you've gone past half the digits. Keep a running list of invalids you added so you don't double count
+    SumAllInvalid = sum_invalid2(Lines, 1),
+    io:format("Part 2: ~p~n", [SumAllInvalid]),
     done.
 
 
+% Part 2 Code
+sum_invalid2(Lines, MinLength) -> sum_invalid2(Lines, MinLength, 0).
+sum_invalid2([], _, Acc) -> Acc;
+sum_invalid2([H|T], MinLength, Acc) ->
+    [Start|[End|_]] = string:split(H, "-", all),
+    io:format("Handling ~p to ~p~n", [Start, End]),
+
+    RangeSum = sum_all_repeating(Start, End, 1),
+    sum_invalid2(T, MinLength, Acc + RangeSum).
+
+
+sum_all_repeating(Start, End, RepeatLength) ->
+    sum_all_repeating(Start, End, RepeatLength, [], 0).
+sum_all_repeating(Start, End, RepeatLength, Seen, Acc) ->
+    NumLength = string:length(End),
+
+    case NumLength of
+        % NumLength < Repeatlength, exit.
+        NL when NL < RepeatLength ->
+            Acc;
+
+        _ ->
+            Curr = lists:sublist(Start, RepeatLength),
+            {RLSum, NewSeen} = sum_all_repeating_length(Start, End, Curr, RepeatLength, Seen, 0),
+            sum_all_repeating(Start, End, RepeatLength + 1, NewSeen, Acc + RLSum)
+    end.
+
+sum_all_repeating_length(Start, End, Curr, RepeatLength, Seen, Acc) ->
+    NumLength = string:length(Start),
+    NumRepeat = NumLength div RepeatLength,
+
+    {CurrNum, _} = string:to_integer(Curr),
+
+    Candidate = string:copies(Curr, NumRepeat),
+    {CandNum, _} = string:to_integer(Candidate),
+
+    IsSeen = lists:member(CandNum, Seen),
+    case IsSeen of
+        true ->
+            CandNum2 = 0;
+        false ->
+            CandNum2 = CandNum
+    end,
+
+    {StartNum, _} = string:to_integer(Start),
+    {EndNum, _} = string:to_integer(End),
+    CurrLength = string:length(Curr),
+
+    case CandNum2 of
+        CN when CN < StartNum ->
+            sum_all_repeating_length(
+                Start,
+                End,
+                integer_to_list(CurrNum + 1),
+                RepeatLength,
+                Seen,
+                Acc
+            );
+        CN when CN > EndNum orelse CurrLength =/= RepeatLength ->
+            {Acc, Seen};
+        _ ->
+            io:format("Taking invalid ~p~n", [CandNum2]),
+            sum_all_repeating_length(
+                Start,
+                End,
+                integer_to_list(CurrNum + 1),
+                RepeatLength,
+                Seen ++ [CandNum2],
+                Acc + CandNum2
+            )
+    end.
+
+
+% Part 1 Code
 sum_invalid(Lines) -> sum_invalid(Lines, 0).
 sum_invalid([], Acc) -> Acc;
 sum_invalid([H|T], Acc) ->
     [Start|[End|_]] = string:split(H, "-", all),
-    io:format("Handling ~p to ~p~n", [Start, End]),
 
     % We can skip any number with an odd number of digits
     % Get first and last invalid numbers based on the range boundaries
@@ -73,8 +153,6 @@ sum_invalid_in_range(Curr, End, Acc) ->
         false ->
             ClosestInvalid = get_next_invalid(Curr, ENum),
             {Invalid, _} = string:to_integer(ClosestInvalid),
-
-            io:format("Invalid to add ~p~n", [Invalid]),
 
             case Invalid of
                 I when I =:= -1 ->
