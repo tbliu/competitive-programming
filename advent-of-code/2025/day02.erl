818a7058199c3484e321f54b5b9eda8dc4e75d5f
Timothy <tim@lumosity.ai>
2025-12-02 09:35:08 -0800

    Get day 2 part 2 test case working

--- advent-of-code/2025/day02.erl
+++ advent-of-code/2025/day02.erl
@@ -11,42 +11,53 @@ main(_) ->
     % - start with first digit; copy it as many times to get it into range. Add to SumInvalid
     % - once you can no longer add digits, take the first two digits slice; copy it as many times to get it into range
     % - repeat until you've gone past half the digits. Keep a running list of invalids you added so you don't double count
-    SumAllInvalid = sum_invalid2(Lines, 1),
+    SumAllInvalid = sum_invalid2(Lines),
     io:format("Part 2: ~p~n", [SumAllInvalid]),
     done.
 
 
 % Part 2 Code
-sum_invalid2(Lines, MinLength) -> sum_invalid2(Lines, MinLength, 0).
-sum_invalid2([], _, Acc) -> Acc;
-sum_invalid2([H|T], MinLength, Acc) ->
+sum_invalid2(Lines) -> sum_invalid2(Lines, 0).
+sum_invalid2([], Acc) -> Acc;
+sum_invalid2([H|T], Acc) ->
     [Start|[End|_]] = string:split(H, "-", all),
     io:format("Handling ~p to ~p~n", [Start, End]),
 
-    RangeSum = sum_all_repeating(Start, End, 1),
-    sum_invalid2(T, MinLength, Acc + RangeSum).
+    RangeSum = sum_all_repeating(Start, End),
+    sum_invalid2(T, Acc + RangeSum).
 
 
-sum_all_repeating(Start, End, RepeatLength) ->
-    sum_all_repeating(Start, End, RepeatLength, [], 0).
-sum_all_repeating(Start, End, RepeatLength, Seen, Acc) ->
-    NumLength = string:length(End),
+sum_all_repeating(Start, End) ->
+    StartLength = string:length(Start),
+    EndLength = string:length(End),
+    {RangeSum, _} = sum_all_repeating(Start, End, 1, StartLength, EndLength, [], 0),
+    RangeSum.
 
-    case NumLength of
-        % NumLength < Repeatlength, exit.
-        NL when NL < RepeatLength ->
-            Acc;
 
-        _ ->
-            Curr = lists:sublist(Start, RepeatLength),
-            {RLSum, NewSeen} = sum_all_repeating_length(Start, End, Curr, RepeatLength, Seen, 0),
-            sum_all_repeating(Start, End, RepeatLength + 1, NewSeen, Acc + RLSum)
+% I feel like we might need a tracking of the desired target length and the slice length
+% to track if it's evenly dividing
+sum_all_repeating(Start, End, TakeFirstN, TargetLength, MaxLength, Seen, Acc) ->
+    case TakeFirstN of
+        _ when TargetLength > MaxLength ->
+            {Acc, Seen};
+        TFN when TFN >= TargetLength div 2 + 1 ->
+            sum_all_repeating(Start, End, 1, TargetLength + 1, MaxLength, Seen, Acc);
+        TFN when TargetLength rem TFN =/= 0 ->
+            sum_all_repeating(Start, End, TakeFirstN + 1, TargetLength, MaxLength, Seen, Acc);
+        TFN ->
+            % io:format("Args: TFN ~p Target Length ~p Max Length ~p~n", [TakeFirstN, TargetLength, MaxLength]),
+            FirstNDigits = min(lists:sublist(Start, TFN), lists:sublist(End, TFN)),
+            NumRepeat = TargetLength div TFN,
+            {RangeSum1, NewSeen1} = sum_all_repeating_length(Start, End, FirstNDigits, NumRepeat, Seen, 0),
+            {RangeSum2, NewSeen2} = sum_all_repeating(Start, End, TFN + 1, TargetLength, MaxLength, NewSeen1, 0),
+            {RangeSum3, NewSeen3} = sum_all_repeating(Start, End, 1, TargetLength + 1, MaxLength, NewSeen2, 0),
+
+            Total = RangeSum1 + RangeSum2 + RangeSum3 + Acc,
+            {Total, NewSeen3}
     end.
 
-sum_all_repeating_length(Start, End, Curr, RepeatLength, Seen, Acc) ->
-    NumLength = string:length(Start),
-    NumRepeat = NumLength div RepeatLength,
 
+sum_all_repeating_length(Start, End, Curr, NumRepeat, Seen, Acc) ->
     {CurrNum, _} = string:to_integer(Curr),
 
     Candidate = string:copies(Curr, NumRepeat),
@@ -62,27 +73,30 @@ sum_all_repeating_length(Start, End, Curr, RepeatLength, Seen, Acc) ->
 
     {StartNum, _} = string:to_integer(Start),
     {EndNum, _} = string:to_integer(End),
-    CurrLength = string:length(Curr),
+
+    % io:format("Start num ~p End num ~p Num Repeat ~p Cand num ~p~n", [StartNum, EndNum, NumRepeat, CandNum]),
 
     case CandNum2 of
         CN when CN < StartNum ->
+            % io:format("Args; NumRepeat ~p Curr ~p Candidate ~p~n", [NumRepeat, Curr, Candidate]),
             sum_all_repeating_length(
                 Start,
                 End,
                 integer_to_list(CurrNum + 1),
-                RepeatLength,
+                NumRepeat,
                 Seen,
                 Acc
             );
-        CN when CN > EndNum orelse CurrLength =/= RepeatLength ->
+        CN when CN > EndNum ->
             {Acc, Seen};
         _ ->
+            % io:format("Args; NumRepeat ~p Curr ~p Candidate ~p~n", [NumRepeat, Curr, Candidate]),
             io:format("Taking invalid ~p~n", [CandNum2]),
             sum_all_repeating_length(
                 Start,
                 End,
                 integer_to_list(CurrNum + 1),
-                RepeatLength,
+                NumRepeat,
                 Seen ++ [CandNum2],
                 Acc + CandNum2
             )
@@ -167,4 +181,4 @@ sum_invalid_in_range(Curr, End, Acc) ->
 read_lines_string() ->
     {ok, Data} = file:read_file("input.txt"),
     Lines = string:split(binary_to_list(Data), ",", all),
-    [L || L <- Lines, L =/= "" ].
+    [string:trim(L) || L <- Lines, L =/= "" ].
