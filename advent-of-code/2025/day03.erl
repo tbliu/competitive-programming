7f2e30a03856b9d83229356de6d00d39a44cf75a
Timothy <tim@lumosity.ai>
2025-12-02 22:34:03 -0800

    Working on day 3 part 2

--- advent-of-code/2025/day03.erl
+++ advent-of-code/2025/day03.erl
@@ -6,17 +6,56 @@ main(_) ->
     MaxJoltage = sum_max_banks(Lines, 2),
     io:format("Part 1: ~p~n", [MaxJoltage]),
 
-    % Part 2 - short circuit by tracking max joltage so far
+    MaxJoltage2 = sum_max_banks_greedy(Lines, 12),
+    io:format("Part 2: ~p~n", [MaxJoltage2]),
     done.
 
 
+% Part 2 code - 98417339375687 too low
+sum_max_banks_greedy(Lines, NumDigits) -> sum_max_banks_greedy(Lines, NumDigits, 0).
+sum_max_banks_greedy([], _, Acc) -> Acc;
+sum_max_banks_greedy([H|T], NumDigits, Acc) ->
+    MaxBankValue = get_max_bank_value_greedy(H, NumDigits),
+    % io:format("Max bank value for ~p is ~p~n", [H, MaxBankValue]),
+    sum_max_banks_greedy(T, NumDigits, Acc + MaxBankValue).
+
+get_max_bank_value_greedy([H|T], NumDigits) ->
+    % Always assume well-formed input; we will always start with the first value
+    {HNum, _} = string:to_integer([H]),
+    get_max_bank_value_greedy(T, NumDigits, HNum, HNum).
+get_max_bank_value_greedy([], _, _, Acc) -> Acc;
+get_max_bank_value_greedy([H|T], NumDigits, LastDigit, Acc) ->
+    AccStr = integer_to_list(Acc),
+    AccLength = string:length(AccStr),
+
+
+    DigitsNeeded = NumDigits - AccLength,
+    DigitsRemaining = string:length(T),
+
+    {HNum, _} = string:to_integer([H]),
+    ReplaceLastDigit = HNum > LastDigit andalso DigitsRemaining >= DigitsNeeded,
+    CanTakeDigit = DigitsNeeded > 0,
+
+    case ReplaceLastDigit of
+        true ->
+            get_max_bank_value_greedy(T, NumDigits, HNum, Acc div 10 * 10 + HNum);
+        false ->
+            case CanTakeDigit of
+                true ->
+                    get_max_bank_value_greedy(T, NumDigits, HNum, Acc * 10 + HNum);
+                false ->
+                    get_max_bank_value_greedy(T, NumDigits, LastDigit, Acc)
+            end
+    end.
+
+
+% Part 1 code
 sum_max_banks(Lines, NumDigits) -> sum_max_banks(Lines, NumDigits, 0).
 sum_max_banks([], _, Acc) -> Acc;
 sum_max_banks([H|T], NumDigits, Acc) ->
     MaxBankValue = get_max_bank_value(H, NumDigits),
     % io:format("Max bank value for ~p is ~p~n", [H, MaxBankValue]),
     sum_max_banks(T, NumDigits, Acc + MaxBankValue).
-
 
 get_max_bank_value(Bank, NumDigits) -> get_max_bank_value(Bank, NumDigits, 0).
 get_max_bank_value([], NumDigits, Acc) ->
