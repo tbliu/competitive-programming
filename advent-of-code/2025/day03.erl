20f27c5c79d367c0e1339d8964321f6a13e16605
Timothy <tim@lumosity.ai>
2025-12-02 21:42:34 -0800

    Make part 1 a bit more extensible

--- advent-of-code/2025/day03.erl
+++ advent-of-code/2025/day03.erl
@@ -3,42 +3,43 @@
 
 main(_) ->
     Lines = read_lines_string(),
-    MaxJoltage = sum_max_banks(Lines),
+    MaxJoltage = sum_max_banks(Lines, 2),
     io:format("Part 1: ~p~n", [MaxJoltage]),
 
+    % Part 2 - short circuit by tracking max joltage so far
     done.
 
 
-sum_max_banks(Lines) -> sum_max_banks(Lines, 0).
-sum_max_banks([], Acc) -> Acc;
-sum_max_banks([H|T], Acc) ->
-    MaxBankValue = get_max_bank_value(H),
+sum_max_banks(Lines, NumDigits) -> sum_max_banks(Lines, NumDigits, 0).
+sum_max_banks([], _, Acc) -> Acc;
+sum_max_banks([H|T], NumDigits, Acc) ->
+    MaxBankValue = get_max_bank_value(H, NumDigits),
     % io:format("Max bank value for ~p is ~p~n", [H, MaxBankValue]),
-    sum_max_banks(T, Acc + MaxBankValue).
+    sum_max_banks(T, NumDigits, Acc + MaxBankValue).
 
 
-get_max_bank_value(Bank) -> get_max_bank_value(Bank, 0).
-get_max_bank_value([], Acc) ->
+get_max_bank_value(Bank, NumDigits) -> get_max_bank_value(Bank, NumDigits, 0).
+get_max_bank_value([], NumDigits, Acc) ->
     Length = string:length(integer_to_list(Acc)),
     case Length of
-        2 ->
+        L when L =:= NumDigits ->
             Acc;
         _ ->  % invalid
             -1
     end;
-get_max_bank_value([H|T], Acc) ->
+get_max_bank_value([H|T], NumDigits, Acc) ->
     Length = string:length(integer_to_list(Acc)),
     {HNum, _} = string:to_integer([H]),
     case Length of
-        L when L =:= 0 orelse L =:= 1 ->
-            Take = get_max_bank_value(T, Acc * 10 + HNum),
-            DontTake = get_max_bank_value(T, Acc),
+        L when L < NumDigits ->
+            Take = get_max_bank_value(T, NumDigits, Acc * 10 + HNum),
+            DontTake = get_max_bank_value(T, NumDigits, Acc),
 
             Value = max(Take, DontTake),
             Value;
-        L when L =:= 2 ->
+        L when L =:= NumDigits ->
             Acc;
-        L when L > 2 ->
+        L when L > NumDigits ->
             -1
     end.
 
